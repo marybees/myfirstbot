c1e809b6607e80e84880ee6c150e8db9486324fd
Mary Baker <marybak@microsoft.com>
2017-03-07 17:53:34 -0500

    Add restify

--- myfirstbot.js
+++ myfirstbot.js
@@ -1,6 +1,7 @@
-var builder     = require('botbuilder');
-var connector   = new builder.ConsoleConnector().listen();
-var bot         = new builder.UniversalBot(connector);   
+var builder     = require('botbuilder')
+var restify     = require('restify')
+var connector   = new builder.ChatConnector()
+var bot         = new builder.UniversalBot(connector)   
 
 bot.dialog("/", [
     session => { builder.Prompts.text(session, "what is your name?") },
@@ -14,3 +15,9 @@ bot.dialog("/", [
             }
     }
 ])
+
+var server = restify.createServer()
+server.listen(3978, function() {    
+    console.log('test bot endpoint at http://localhost:3978/api/messages')
+})
+server.post('/api/messages', connector.listen())
